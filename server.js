53bbf0411dbf86c4befaf6a19714623e2582837f
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-11-26 18:58:13 +0500

    emit end ride everywhere

--- server.js
+++ server.js
@@ -150,13 +150,13 @@ io.on('connection', (socket) => {
         })
         database.ref('finished').child(driver.key).set(driver)
         socket.join('available')
-        io.in(driver.key).emit('end ride', driver)
-        // io.of('/').in(driver.key).clients((error, socketIds) => {
-        //     if (error) throw error;
+        io.emit('end ride', driver)
+        io.of('/').in(driver.key).clients((error, socketIds) => {
+            if (error) throw error;
 
-        //     socketIds.forEach(socketId => io.sockets.sockets[socketId].leave(driver.key));
+            socketIds.forEach(socketId => io.sockets.sockets[socketId].leave(driver.key));
 
-        // });
+        });
     });
 
     // when the driver or user disconnects
